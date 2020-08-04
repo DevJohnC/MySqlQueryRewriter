ba2a4100cf1e966d01cbcf30a129c36d9193da4f
John Carruthers <johnc@frag-labs.com>
2020-08-04 12:42:51 -0500

    Add backtick support on IdentifierRewriteRule.

--- MySqlQueryRewriter/IdentifierRewriteRule.cs
+++ MySqlQueryRewriter/IdentifierRewriteRule.cs
@@ -4,18 +4,35 @@ namespace MySqlQueryRewriter
 {
 	public class IdentifierRewriteRule : RewriteRule
 	{
-		public IdentifierRewriteRule(string identifier, string replacement)
+		public IdentifierRewriteRule(string identifier, string replacement, bool replaceBacktickQuotedIdentifiers = true)
 		{
 			Identifier = identifier;
 			Replacement = replacement;
+			_replaceBacktickQuotedIdentifiers = replaceBacktickQuotedIdentifiers;
+
+			if (replaceBacktickQuotedIdentifiers)
+			{
+				_backtickQuotedIdentifier = $"`{identifier}`";
+				_backtickQuotedReplacement = $"`{replacement}`";
+			}
 		}
 
 		public string Identifier { get; }
 		public string Replacement { get; }
 
+		private readonly bool _replaceBacktickQuotedIdentifiers;
+
+		private readonly string _backtickQuotedIdentifier = string.Empty;
+
+		private readonly string _backtickQuotedReplacement = string.Empty;
+
 		public override void ConfigureQueryVisitor(MySqlQueryVisitor queryVisitor)
 		{
 			queryVisitor.AddTerminalNodeRewriter(parsers.MySQLLexer.IDENTIFIER, RewriteIdentifier);
+			if (_replaceBacktickQuotedIdentifiers)
+			{
+				queryVisitor.AddTerminalNodeRewriter(parsers.MySQLLexer.BACK_TICK_QUOTED_ID, RewriteBacktickQuotedIdentifier);
+			}
 		}
 
 		private RewriteResult RewriteIdentifier(MySqlQueryWriter writer, ITerminalNode node)
@@ -24,6 +41,17 @@ namespace MySqlQueryRewriter
 			if (string.Equals(identifier, Identifier, System.StringComparison.OrdinalIgnoreCase))
 			{
 				identifier = Replacement;
+			}
+			writer.WriteToken(identifier);
+			return RewriteResult.HaltProcessing;
+		}
+
+		private RewriteResult RewriteBacktickQuotedIdentifier(MySqlQueryWriter writer, ITerminalNode node)
+		{
+			var identifier = node.GetText();
+			if (string.Equals(identifier, _backtickQuotedIdentifier, System.StringComparison.OrdinalIgnoreCase))
+			{
+				identifier = _backtickQuotedReplacement;
 			}
 			writer.WriteToken(identifier);
 			return RewriteResult.HaltProcessing;
